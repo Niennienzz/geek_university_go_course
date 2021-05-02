425887254661f7b7ebfadf6368c17fbecbd2e27a
Joe Zhou <zhehui.zhou@outlook.com>
2021-05-01 21:12:16 -0400

    week 3 assignment x2: improve server, add comments.

--- week_03/main.go
+++ week_03/main.go
@@ -13,6 +13,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+// 将要在 errgroup.Group 里用到的四个成员
 var (
 	timer             *Timer
 	server1           *Server
@@ -20,8 +21,9 @@ var (
 	interruptListener *InterruptListener
 )
 
+// 初始化上面四个成员
 func init() {
-	timer = NewTimer(time.Second * 10)
+	timer = NewTimer(time.Minute)
 
 	{
 		mux1 := http.NewServeMux()
@@ -43,11 +45,14 @@ func init() {
 }
 
 func main() {
+	// 给定一个全局上下文，若主函数退出则所有子上下文必须退出
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// 建立一个 errgroup.Group，并把成员上下文绑定在一起
 	group, ctx := errgroup.WithContext(ctx)
 
+	// 利用 errgroup.Group 分别并发四个成员
 	group.Go(func() error {
 		return timer.Run(ctx)
 	})
@@ -64,11 +69,18 @@ func main() {
 		return interruptListener.Run(ctx)
 	})
 
+	// 等待所有成员返回
+	// 若有成员出错导致返回，打印第一个出错的成员的错误
 	if err := group.Wait(); err != nil {
 		log.Printf("first error in the group: %v", err)
 		return
 	}
 }
+
+// 以下三个结构体的设计编写均满足以下原则
+//  - 如果不是特殊情况，把并发控制权交给调用者
+//  - 如果收到 ctx.Done 中的消息，则证明同一个 errgroup.Group 的其他成员出错了，让自己优雅关闭
+//  - 反之，如果自己出错需要关闭，则通过返回错误来通知 errgroup.Group 里的其他成员
 
 type Server struct {
 	server http.Server
@@ -85,16 +97,42 @@ func NewServer(label, addr string, handler http.Handler) *Server {
 	}
 }
 
+// Server 监听 ctx.Done
+// 若 ctx 取消则自己也取消
+// 若自己出错，则通过返回错误来通知其他成员
 func (s *Server) Run(ctx context.Context) error {
+	ch := make(chan error)
+
+	// 这里是一个特殊情况，因为 ListenAndServe 是阻塞的
+	// 所以在一个新的 goroutine 里面运行它，并监听它的错误
 	go func() {
-		<-ctx.Done()
-		log.Printf("%s shutdown by context cancellation", s.label)
-		s.server.Shutdown(context.Background())
+		ch <- s.server.ListenAndServe()
 	}()
 
-	return s.server.ListenAndServe()
+	// 若监听到 ctx.Done 取消，则关闭自己的服务
+	// 然而关闭服务的过程也有可能比较耗时，并且有可能出错
+	// 所以我们给 Shutdown 方法一个新的带有超时的上下文
+	// 同时如果 Shutdown 方法出错，此时程序已经在退出的阶段了，我们通过简单输出日志的方式处理这个错误
+	//
+	// 若监听到自己的 ListenAndServe 出错，则通过返回错误来通知其他成员
+	select {
+	case <-ctx.Done():
+		log.Printf("%s shutdown by context cancellation", s.label)
+		ctxShutdown, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		if err := s.server.Shutdown(ctxShutdown); err != nil {
+			log.Printf("%s shutdown with error: %v", s.label, err)
+		}
+	case err := <-ch:
+		return fmt.Errorf("%s shutdown with error: %w", s.label, err)
+	}
+
+	return nil
 }
 
+// Timer 监听 ctx.Done
+// 若 ctx 取消则自己也取消
+// 若定的时间到了，则自己通过返回错误来通知其他成员
 type Timer struct {
 	duration time.Duration
 }
@@ -113,6 +151,9 @@ func (t Timer) Run(ctx context.Context) error {
 	return nil
 }
 
+// InterruptListener 监听 ctx.Done
+// 若 ctx 取消则自己也取消
+// 若收到系统中断命令 Ctrl+C，则自己通过返回错误来通知其他成员
 type InterruptListener struct{}
 
 func NewInterruptListener() *InterruptListener {
